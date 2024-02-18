5b30a3940cf3d5b47f487f1db4e704cec8074347
Shawn Sun <859005086@qq.com>
2024-02-18 18:47:27 +0800

    fix build

--- iosbridge/build.rs
+++ iosbridge/build.rs
@@ -1,11 +1,17 @@
+use std::{env, fs, io};
 
 fn main() {
+    let target = env::var("TARGET").expect("Cargo build scripts always have TARGET");
+    let host = env::var("HOST").expect("Cargo build scripts always have HOST");
+    let target_os = get_os_from_triple(target.as_str()).unwrap();
+    // 指定静态库的路径
+    if target_os.contains( "ios" ){
+        println!("cargo:rustc-link-search=native=/Users/xigsun/Documents/repo/candle/iosbridge/");
+        println!("cargo:rustc-link-search=framework=/Users/xigsun/Documents/repo/candle/iosbridge/");
+        println!("cargo:rustc-flags=-l framework=SDL2");
+    }
+}
 
-    #[cfg(target_os = "ios")]
-    println!("cargo:rustc-link-search=native=/Users/xigsun/Documents/repo/candle/iosbridge/");
-    #[cfg(target_os = "ios")]
-    println!("cargo:rustc-link-search=framework=/Users/xigsun/Documents/repo/candle/iosbridge/");
-    #[cfg(target_os = "ios")]
-    println!("cargo:rustc-flags=-l framework=SDL2");
-
+fn get_os_from_triple(triple: &str) -> Option<&str> {
+    triple.splitn(3, "-").nth(2)
 }
